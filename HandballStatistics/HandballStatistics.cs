89c365a16ae2b92068ba4e0a8ef66c70f0e4b5ce
albenarammou\nessos <albenarammou@gmail.com>
2018-02-02 12:25:18 +0200

    commit handball statistics

--- HandballStatistics/HandballStatistics.cs
+++ HandballStatistics/HandballStatistics.cs
@@ -11,13 +11,13 @@ namespace HandballStatistics
         static void Main(string[] args)
         {
 
-            string[,] InputLines = new string[50,8];
+            string[] InputLines = new string[8];
+            List<Team> TeamList = new List<Team>();
+            //Team TeamMember  = new Team();
             var input = "";
             var x = 0;
             var y = 0;
 
-            //Console.WriteLine("Press any key to continue...");
-            //Console.ReadKey();
             for (int i=0; i<50; i++)
             {
                 x = 0;
@@ -30,7 +30,13 @@ namespace HandballStatistics
                     if (x < 2)
                     {
                         SepIndex = input.IndexOf("|");
-                        InputLines[y, x] = input.Substring(0, SepIndex - 1);
+                        InputLines[x] = input.Substring(0, SepIndex - 1);
+                        Team TeamMember = new Team();
+                        TeamMember.TeamIndex = y + x;
+                        TeamMember.TeamName = (input.Substring(0, SepIndex - 1));
+                        TeamMember.TeamWins = 0;
+                        TeamMember.TeamOpponents = "";
+                        TeamList.Add(TeamMember);
                         input = input.Substring(SepIndex + 2, input.Length - SepIndex - 2);
                         x += 1;
                         continue;
@@ -39,8 +45,8 @@ namespace HandballStatistics
                     {
                         SepIndex = input.IndexOf("|");
                         PointIndex = input.IndexOf(":");
-                        InputLines[y, x] = input.Substring(0, PointIndex);
-                        InputLines[y, x+1] = input.Substring(PointIndex+1, SepIndex - 2);
+                        InputLines[x] = input.Substring(0, PointIndex);
+                        InputLines[x+1] = input.Substring(PointIndex+1, SepIndex - 2);
                         input = input.Substring(SepIndex + 2, input.Length - SepIndex - 2);
                         x += 1;
                         continue;
@@ -48,33 +54,43 @@ namespace HandballStatistics
                     else if (x == 3)
                     {
                         PointIndex = input.IndexOf(":");
-                        InputLines[y, x+1] = input.Substring(0,PointIndex);
-                        InputLines[y, x+2] = input.Substring(PointIndex+1, input.Length- PointIndex-1);
+                        InputLines[x+1] = input.Substring(0,PointIndex);
+                        InputLines[x+2] = input.Substring(PointIndex+1, input.Length- PointIndex-1);
 
-                        if ((int.Parse(InputLines[y, 2]) + int.Parse(InputLines[y, 5])) > (int.Parse(InputLines[y, 3]) + int.Parse(InputLines[y, 4])))
+                        if ((int.Parse(InputLines[2]) + int.Parse(InputLines[5])) > (int.Parse(InputLines[3]) + int.Parse(InputLines[4])))
                         {
-                            InputLines[y, 6] = "W";
-                            InputLines[y, 7] = "L";
+                            InputLines[6] = "W";
+                            InputLines[7] = "L";
+                            TeamList.ElementAt(2 * y).TeamWins += 1;
+                            //TeamList.ElementAt(2*y).TeamOpponents = TeamList.ElementAt(2*y+1).TeamName;
                         }
-                        else if ((int.Parse(InputLines[y, 2]) + int.Parse(InputLines[y, 5])) < (int.Parse(InputLines[y, 3]) + int.Parse(InputLines[y, 4])))
+                        else if ((int.Parse(InputLines[2]) + int.Parse(InputLines[5])) < (int.Parse(InputLines[3]) + int.Parse(InputLines[4])))
                         {
-                            InputLines[y, 6] = "L";
-                            InputLines[y, 7] = "W";
+                            InputLines[6] = "L";
+                            InputLines[7] = "W";
+                            TeamList.ElementAt(2 * y + 1).TeamWins += 1;
+                            //TeamList.ElementAt(2*y+1).TeamOpponents= TeamList.ElementAt(2*y).TeamName;
                         }
                         else
                         {
-                            if (int.Parse(InputLines[y, 5]) > int.Parse(InputLines[y, 3]))
+                            if (int.Parse(InputLines[5]) > int.Parse(InputLines[3]))
                             {
-                                InputLines[y, 6] = "W";
-                                InputLines[y, 7] = "L";
+                                InputLines[6] = "W";
+                                InputLines[7] = "L";
+                                TeamList.ElementAt(2 * y).TeamWins += 1;
                             }
-                            else if (int.Parse(InputLines[y, 5]) < int.Parse(InputLines[y, 3]))
+                            else if (int.Parse(InputLines[5]) < int.Parse(InputLines[3]))
                             {
-                                InputLines[y, 6] = "L";
-                                InputLines[y, 7] = "W";
+                                InputLines[6] = "L";
+                                InputLines[7] = "W";
+                                TeamList.ElementAt(2 * y + 1).TeamWins += 1;
                             }
                             else { }
                         }
+
+                        TeamList.ElementAt(2*y).TeamOpponents = TeamList.ElementAt(2*y+1).TeamName;
+                        TeamList.ElementAt(2*y+1).TeamOpponents = TeamList.ElementAt(2*y).TeamName;
+
                         input = "";
                         y += 1;
 
@@ -84,47 +100,34 @@ namespace HandballStatistics
             }
 
 
+            //TeamList.Sort(TeamMember.TeamName);
 
-            for (int j = 0; j < InputLines.Length; j++)
+            //for (int i = 0; i < TeamList.Count; i++)
+            //{
+            //    TeamList.Select(n => n.TeamOpponents).Where(n => (n.TeamName == TeamList.ElementAt(i).TeamName));
+            //}
+
+
+            for (int i =0; i < TeamList.Count; i++)
             {
-                var count = 0;
-                var Oponents = "";
-                for (int i = j; i < InputLines.Length; i++)
-                {
-
-                    if ((InputLines[j, 0] != "") && (InputLines[j, 0] == InputLines[i, 0]))
-                    {
-                        if ((InputLines[i, 6]) == "W")
-                        {
-                            count += 1;
-                            if (count > 1) { Oponents = Oponents + ", " + InputLines[i, 1]; }
-                            else { Oponents = Oponents + InputLines[i, 1]; }
-                            InputLines[i, 0] = "";
-                        }
-
-                    }
-                    if ((InputLines[j, 0] != "") && (InputLines[j, 0] == InputLines[i, 1]))
-                    {
-                        if ((InputLines[i, 7]) == "W")
-                        {
-                            count += 1;
-                            if (count > 1) { Oponents = Oponents + ", " + InputLines[i, 0]; }
-                            else { Oponents = Oponents + InputLines[i, 0]; }
-                            InputLines[i, 1] = "";
-                        }
-
-                    }
-                    Console.WriteLine(InputLines[j,0]);
-                    Console.WriteLine("Wins:" + count.ToString());
-                    Console.WriteLine("opponents:" + Oponents);
-
-
-                }
+                Console.WriteLine(TeamList.ElementAt(i).TeamName);
+                Console.WriteLine("-Wins:" + TeamList.ElementAt(i).TeamWins.ToString());
+                Console.WriteLine("-Opponents:" + TeamList.ElementAt(i).TeamOpponents);
             }
-        
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
 
-                
-            }
         }
+
+
+        public class Team
+        {
+            public int TeamIndex { get; set; }
+            public string TeamName { get; set; }
+            public int TeamWins { get; set; }
+            public string TeamOpponents { get; set; }
+        }
+
+    }
     }
 
